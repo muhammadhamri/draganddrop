d5e9c99176b8d998b0f1d45d7a3f0814f204b070
muhammad hamri <muhammadhamri1977@gmail.com>
2024-01-06 08:42:04 +0700

    kode

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
-// let lists = document.getElementsByClassName("list");
-// let rightBox = document.getElementById("right");
-// let leftBox = document.getElementById("left");
-
-// for (list of lists) {
-//   list.addEventListener("dragstart", function (e) {
-//     let selected = e.target;
-
-//     rightBox.addEventListener("dragover", function (e) {
-//       e.preventDefault();
-//     });
-
-//     rightBox.addEventListener("drop", function (e) {
-//       rightBox.appendChild(selected);
-//       selected = null;
-//     });
-
-//     leftBox.addEventListener("dragover", function (e) {
-//       e.preventDefault();
-//     });
-
-//     leftBox.addEventListener("drop", function (e) {
-//       leftBox.appendChild(selected);
-//       selected = null;
-//     });
-//   });
-// }
-
 let lists = document.getElementsByClassName("list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
-let selected = null;
 
-for (let list of lists) {
-  list.addEventListener("touchstart", function (e) {
-    selected = e.target;
-  });
+for (list of lists) {
+  list.addEventListener("dragstart", function (e) {
+    let selected = e.target;
 
-  rightBox.addEventListener("touchmove", function (e) {
-    e.preventDefault();
-  });
+    rightBox.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
 
-  rightBox.addEventListener("touchend", function (e) {
-    if (selected) {
+    rightBox.addEventListener("drop", function (e) {
       rightBox.appendChild(selected);
       selected = null;
-    }
-  });
+    });
 
-  leftBox.addEventListener("touchmove", function (e) {
-    e.preventDefault();
-  });
+    leftBox.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
 
-  leftBox.addEventListener("touchend", function (e) {
-    if (selected) {
+    leftBox.addEventListener("drop", function (e) {
       leftBox.appendChild(selected);
       selected = null;
-    }
+    });
   });
 }
+
+// let lists = document.getElementsByClassName("list");
+// let rightBox = document.getElementById("right");
+// let leftBox = document.getElementById("left");
+// let selected = null;
+
+// for (let list of lists) {
+//   list.addEventListener("touchstart", function (e) {
+//     selected = e.target;
+//   });
+
+//   rightBox.addEventListener("touchmove", function (e) {
+//     e.preventDefault();
+//   });
+
+//   rightBox.addEventListener("touchend", function (e) {
+//     if (selected) {
+//       rightBox.appendChild(selected);
+//       selected = null;
+//     }
+//   });
+
+//   leftBox.addEventListener("touchmove", function (e) {
+//     e.preventDefault();
+//   });
+
+//   leftBox.addEventListener("touchend", function (e) {
+//     if (selected) {
+//       leftBox.appendChild(selected);
+//       selected = null;
+//     }
+//   });
+// }
