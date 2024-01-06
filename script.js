196edbaa4124c1f725b90ad0bee78ff5f6f34be5
muhammad hamri <muhammadhamri1977@gmail.com>
2024-01-06 08:31:44 +0700

    layarsentuh

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
+// let lists = document.getElementsByClassName("list");
+// let rightBox = document.getElementById("right");
+// let leftBox = document.getElementById("left");
+
+// for (list of lists) {
+//   list.addEventListener("dragstart", function (e) {
+//     let selected = e.target;
+
+//     rightBox.addEventListener("dragover", function (e) {
+//       e.preventDefault();
+//     });
+
+//     rightBox.addEventListener("drop", function (e) {
+//       rightBox.appendChild(selected);
+//       selected = null;
+//     });
+
+//     leftBox.addEventListener("dragover", function (e) {
+//       e.preventDefault();
+//     });
+
+//     leftBox.addEventListener("drop", function (e) {
+//       leftBox.appendChild(selected);
+//       selected = null;
+//     });
+//   });
+// }
+
 let lists = document.getElementsByClassName("list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
+let selected = null;
 
-for (list of lists) {
-  list.addEventListener("dragstart", function (e) {
-    let selected = e.target;
+for (let list of lists) {
+  list.addEventListener("touchstart", function (e) {
+    selected = e.target;
+  });
 
-    rightBox.addEventListener("dragover", function (e) {
-      e.preventDefault();
-    });
+  rightBox.addEventListener("touchmove", function (e) {
+    e.preventDefault();
+  });
 
-    rightBox.addEventListener("drop", function (e) {
+  rightBox.addEventListener("touchend", function (e) {
+    if (selected) {
       rightBox.appendChild(selected);
       selected = null;
-    });
+    }
+  });
 
-    leftBox.addEventListener("dragover", function (e) {
-      e.preventDefault();
-    });
+  leftBox.addEventListener("touchmove", function (e) {
+    e.preventDefault();
+  });
 
-    leftBox.addEventListener("drop", function (e) {
+  leftBox.addEventListener("touchend", function (e) {
+    if (selected) {
       leftBox.appendChild(selected);
       selected = null;
-    });
+    }
   });
 }
